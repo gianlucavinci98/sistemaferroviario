fdf284d374ce4c56e42cff0dcee8613648ee2084
Andrea <Andrea@Rammazzo.station>
2022-11-04 09:35:05 +0100

    Cancellazione testTrenoBuilder

--- SistemaFerroviarioDBLogic/test/corso/test/testTrenoBuilder.java
+++ SistemaFerroviarioDBLogic/test/corso/test/testTrenoBuilder.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.corso.builder.TrenoBuilder;
-import com.corso.builder.TrenoBuilder.Builder;
+
 
 import corso.DAOs.TrenoDAO;
 import corso.model.Treno;
@@ -14,27 +14,15 @@ import corso.spring.Beans;
 
 public class testTrenoBuilder {
 
-	public static void main(String[] args) {
-		testBuilderTreno();
-		try {
+	public static void main(String[] args) throws Exception {
 			testAddTreno();
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
 
-	}
 
 	private static void testAddTreno() throws Exception {
 		BeanFactory factory = new AnnotationConfigApplicationContext(Beans.class);
 		TrenoDAO dao = factory.getBean("trenoDAO",TrenoDAO.class);
 		dao.add("HCCC");
-	}
-
-	private static void testBuilderTreno() {
-		Builder tb = new Builder();
-		//TrenoBuilder t = tb.build("HPPP");
-		
-	//	System.out.println(t.show());
 	}
 
 }
